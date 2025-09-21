6396795e458b78beb4c7366998f89d653f37f252
Bfayed <Bavlyayman370@gmail.com>
2025-09-21 14:42:48 -0500

    fixed issue with the router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";     // <-- this must exist and be in /src
+import App from "./App";   
+import "./index.css";
 
-import { BrowserRouter } from "react-router-dom";
+
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
